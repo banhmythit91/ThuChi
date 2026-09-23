761f58e104699adc8074bf2b728bda1a392ade7e
Nguyễn Thị Hà Phương <nhoemvol3@gmail.com>
2026-09-23 16:06:58 +0700

    Set Firebase configuration values
    Updated Firebase configuration with real credentials.

--- firebase-config.js
+++ firebase-config.js
@@ -6,10 +6,10 @@
 //  mật khẩu đăng nhập và Rules của Firestore.
 // =====================================================================
 window.FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDHyvIMyg9Rvc7c0VOVOMu9wRzhUHAGp1c",
+  authDomain: "so-thu-chi-12d96.firebaseapp.com",
+  projectId: "so-thu-chi-12d96",
+  storageBucket: "so-thu-chi-12d96.firebasestorage.app",
+  messagingSenderId: "665256299000",
+  appId: "1:665256299000:web:5d8a454fadb602caa1ae0d"
 };
